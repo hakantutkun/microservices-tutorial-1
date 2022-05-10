2051580c42ec7587ac11300a7c417f5b90fa2c8c
hakan <hakan@DESKTOP-7IJG156.local>
2022-05-10 23:21:52 +0300

    Test için yazılan kısımlar kaldırıldı.

--- Play.Catalog/src/Play.Catalog.Service/Controllers/ItemsController.cs
+++ Play.Catalog/src/Play.Catalog.Service/Controllers/ItemsController.cs
@@ -15,8 +15,6 @@ namespace Play.Catalog.Service.Controllers
     {
         private readonly IRepository<Item> itemsRepository;
 
-        private static int requestCounter = 0;
-
         public ItemsController(IRepository<Item> itemsRepository)
         {
             this.itemsRepository = itemsRepository;
@@ -26,27 +24,8 @@ namespace Play.Catalog.Service.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<ItemDto>>> GetAsync()
         {
-            requestCounter++;
-
-            Console.WriteLine($"Request {requestCounter} : Starting...");
-
-            if(requestCounter <= 2)
-            {
-                Console.WriteLine($"Request {requestCounter} : Delaying...");
-                await Task.Delay(TimeSpan.FromSeconds(10));
-            }
-
-            if(requestCounter <= 4)
-            {
-                Console.WriteLine($"Request {requestCounter} : 500 (Internal Server Error).");
-                return StatusCode(500);
-            }
-
             var items = (await itemsRepository.GetAllAsync())
                         .Select(item => item.AsDto());
-
-            
-            Console.WriteLine($"Request {requestCounter}: 200 (OK)");
 
             return Ok(items);
         }
